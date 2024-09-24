8c6305fe299a016a9f99800d0fb90aa211af7309
James Anderson <145408914+JamesAnderson30@users.noreply.github.com>
2024-09-24 15:39:56 -0400

    Update post_routes.py

--- app/api/post_routes.py
+++ app/api/post_routes.py
@@ -2,6 +2,7 @@ from flask import Blueprint, jsonify
 from flask_login import login_required
 from app.models import Post
 
+
 post_routes = Blueprint('post', __name__)
 
 # Get Post Information
@@ -13,4 +14,23 @@ def post(id):
 # POST new Post Information
 @category_routes.route("/new")
 def new_post():
-    
+     # Create Question
+    form = PostForm()
+    form['csrf_token'].data = request.cookies['csrf_token']
+    if form.validate_on_submit():
+        post = Post(
+            body=form.data['body'],
+            subject=form.data['subject'],
+            user_id=current_user.id
+        )
+        db.session.add(post)
+        db.session.commit()
+        res = {
+            "id": question.id,
+            "question": question.question,
+            "subject": question.subject,
+            "user_id": current_user.id
+        }
+        return jsonify(res), 201
+    else:
+        return form.errors, 401
