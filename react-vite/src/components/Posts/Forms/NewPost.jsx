04e4c9e517bb30591a19ba0c81412cd25c71d659
James Anderson <145408914+JamesAnderson30@users.noreply.github.com>
2024-10-14 12:37:08 -0400

    Update NewPost.jsx

--- react-vite/src/components/Posts/Forms/NewPost.jsx
+++ react-vite/src/components/Posts/Forms/NewPost.jsx
@@ -1,9 +1,10 @@
-import { useState } from "react"
+import { useState, useEffect } from "react"
 import { sendPost } from "../../../redux/post";
 import { useDispatch } from "react-redux";
 function NewPostForm(topic_id, addPost){
 
     const [subject, setSubject] = useState("");
+    const [isDisabled, setIsDisabled] = useState("");
     const [body, setBody] = useState("");
     const [errors, setErrors] = useState([])
     const [errorsHidden, setErrorsHidden] = useState(true)
@@ -18,14 +19,20 @@ function NewPostForm(topic_id, addPost){
             await dispatch(sendPost({body, topic_id}))
         }
     }
+
+    useEffect(()=>{
+        if(body.length > 0) setIsDisabled(false)
+        else setIsDisabled(true)
+    }, [dispatch, body])
+    
     return(
         <form onSubmit={handleSubmit}>
             <input type="hidden" name="topic_id" value={topic_id} />
             <label>
-                Body
+                Author a new Post: 
                 <input type="text" value={body} onChange={(e)=>setBody(e.target.value)} />
             </label>
-            <button type="submit">Post!</button>
+            <button disabled={isDisabled} type="submit">Post!</button>
             <span hidden={errorsHidden}>{errors.map((error)=>{return <span>{error}</span>})}</span>
         </form>
     )
