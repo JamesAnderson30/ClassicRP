4164c6fe7bf77ebc3da144215953127193652552
James Anderson <jamesanderson_18@hotmail.com>
2024-10-16 13:29:03 -0400

    styling

--- react-vite/src/components/Navigation/Navigation.jsx
+++ react-vite/src/components/Navigation/Navigation.jsx
@@ -21,8 +21,11 @@ function Navigation() {
       </ul>
       <div className="Logo">ClassicRP</div>
       <div className="NavButtons">
-        <ul>
-          <li>
+        <svg viewBox="0 0 800 800" xmlns="http://www.w3.org/2000/svg">
+          <line x1="621" y1="332" x2="362" y2="48" stroke="#70AEDB" stroke-width="25"/>
+        </svg>
+        <ul className="NavList">
+          <li className="NavButton">
             Test1
           </li>
           <li>
