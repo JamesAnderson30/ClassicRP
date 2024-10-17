0c779c92f525eac1f14c2db3ad3a54d68f16d5f5
James Anderson <jamesanderson_18@hotmail.com>
2024-10-17 11:36:34 -0400

    styling

--- react-vite/src/components/Users/LoginSignup.jsx
+++ react-vite/src/components/Users/LoginSignup.jsx
@@ -1,19 +1,32 @@
 import { useSelector } from "react-redux";
+import defaultAvatar from "../../../media/default-user.png"
 
 function LoginSignup(){
-    const sessionUser = useSelector((state) => state.session.user);
+    const user = useSelector((state) => state.session.user);
+  
     //IF USER NOT LOGGED IN
-    if(!sessionUser){
+    if(!user){
         return (
             <div id="LoginDiv">
                 <button>Login</button> or <button>Signup!</button>
             </div>
         )
     }
+    
     //IF USER IS
+    //check for default profile image
+    
+
     return (
         <div id="UserInformation">
-            {sessionUser.email}
+            <img 
+                src={user.profilePicture}
+                onError={({ currentTarget }) => {
+                currentTarget.onerror = null; // prevents looping
+                currentTarget.src=defaultAvatar;
+             }} />
+            <p>Welcome {user.username}!</p>
+            
         </div>
     )
 }
