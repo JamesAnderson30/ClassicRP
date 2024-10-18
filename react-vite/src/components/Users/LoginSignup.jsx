19ce3a82faf485d2e6a1cac34ee89f772d56ee22
James Anderson <jamesanderson_18@hotmail.com>
2024-10-18 12:11:33 -0400

    styling

--- react-vite/src/components/Users/LoginSignup.jsx
+++ react-vite/src/components/Users/LoginSignup.jsx
@@ -1,6 +1,7 @@
 import { useDispatch, useSelector } from "react-redux";
 import { useState } from "react";
 import defaultAvatar from "../../../media/default-user.png"
+import { thunkLogout, thunkLogin } from "../../redux/session";
 import "./LoginSingup.css";
 function LoginSignup(){
     const [email, setEmail] = useState("");
