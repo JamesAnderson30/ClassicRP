46d4dd426c13c073bb315dd046977682ea5b4905
James Anderson <jamesanderson_18@hotmail.com>
2024-11-18 20:02:14 -0500

    change migration  to  hotfix

--- migrations/versions/20240922_152449_adding_tables.py
+++ migrations/versions/20240922_152449_adding_tables.py
@@ -74,9 +74,8 @@ def downgrade():
         batch_op.drop_column('role')
         batch_op.drop_column('profilePicture')
 
-    print("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
     op.drop_table('category')
     op.drop_table('topic')
     op.drop_table('post')
-    op.drop_table('topic_profile')
+    # op.drop_table('topic_profile')
     # ### end Alembic commands ###
